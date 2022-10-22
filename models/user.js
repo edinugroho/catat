574892024db5e4b4feb30ef089aaedde1e2b6eae
edinugroho <edidwinugroho80@gmail.com>
2022-10-22 23:18:32 +0700

    fix: hide password

--- models/user.js
+++ models/user.js
@@ -19,6 +19,9 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+    defaultScope: {
+      attributes: { exclude: ['password'] },
+    }
   });
   return User;
 };
