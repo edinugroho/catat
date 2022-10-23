e30a482f45aef910fd2f8956d4525373e1f28f86
edinugroho <edidwinugroho80@gmail.com>
2022-10-23 10:30:04 +0700

    feat: update transaction

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -31,6 +31,45 @@ const store = (req, res) => {
   });
 }
 
+const update = (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.mapped() });
+  }
+
+  model.Transaction.findByPk(req.params.id).then(result => {
+    if (!result) {
+      res.status(404);
+      res.json({
+        "status" : "error",
+        "message" : "Transaction not found"
+      });
+    } else {
+      result.set({
+        amount : req.body.amount,
+        notes : req.body.notes,
+        date : req.body.date,
+        type : req.body.type,
+      })
+      result.save().then(() => {
+        res.status(201);
+        res.json({
+          "status" : "success",
+          "message" : "Transaction updated"
+        });
+      }).catch((err) => {
+        res.status(422);
+        res.json({
+          "status" : "error",
+          "message" : err.message
+        });
+      });
+    } 
+  });
+}
+
 module.exports = {
-  store
+  store,
+  update
 }
--- routes/transaction.js
+++ routes/transaction.js
@@ -4,9 +4,11 @@ const validator = require('../validator/transaction')
 const passport = require('passport');
 
 const {
-  store
+  store,
+  update
 } = require('../controllers/transaction');
 
 router.post('/', passport.authenticate('jwt', { session: false }), validator.store, store);
+router.patch('/:id', passport.authenticate('jwt', { session: false }), validator.store, update);
 
 module.exports = router;
